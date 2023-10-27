c68c2315221af5cbd3b9c4ec873f9d22f09288b5
LucasLoka <110735593+LucasLoka@users.noreply.github.com>
2023-10-27 16:52:06 -0300

    Update Home.jsx

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,7 +1,31 @@
-import React from "react";
+import React from 'react';
 
-const Home = () =>{
+function Home() {
     return(
-        <></>
+        <div className="geral">
+            <div className='cabeça'>
+                <h1 className='title'>Bio Wild</h1>
+                <img className='logoo' src="./src/assets/logo1.png" alt="Bio Wild Logo" />
+            </div>
+
+
+            <div>
+                <h2>Denúncias Anônimas</h2>
+                <p>Para a proteção do nosso meio ambiente faca sua denúncia de forma anônima.</p>
+            </div>
+
+
+
+
+
+
+
+
+
+
+        </div>
+        
     )
 }
+
+export default Home
