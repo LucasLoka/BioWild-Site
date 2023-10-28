978108ada8172a5c363a0d1d9e46f6a099d4bdd8
LucasLoka <110735593+LucasLoka@users.noreply.github.com>
2023-10-27 23:42:05 -0300

    Update Home.jsx

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -9,14 +9,21 @@ function Home() {
             </div>
 
 
-            <div>
+            <div className="bixo">
                 <h2>Denúncias Anônimas</h2>
                 <p>Para a proteção do nosso meio ambiente faca sua denúncia de forma anônima.</p>
+                <button onClick={() => {}}>Denuncie!!</button>
             </div>
 
+            <div className='oiiii'>
+                <h1>Quem somos?</h1>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                    Quam possimus ducimus labore a, cum rerum sapiente eum. 
+                    Maiores dolor amet consequatur, totam sint saepe voluptatibus esse sapiente,
+                    exercitationem, est sequi?</p>
+            </div>
 
-
-
+            
 
 
 
