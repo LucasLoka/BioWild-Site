02175ccba2f55c6d3c0cc614d90e0ddba49a861b
LucasLoka <110735593+LucasLoka@users.noreply.github.com>
2023-11-03 16:39:08 -0300

    Update Chatbot.jsx

--- src/components/Chatbot/Chatbot.jsx
+++ src/components/Chatbot/Chatbot.jsx
@@ -1,8 +1,16 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 function Chatbot(){
     return(
-        <></>
+        <div>
+            <h1>Chatbot</h1>
+
+            <div>
+                <Link to='/'><button>Voltar</button></Link>
+            </div>
+        </div>
+        
     )
 }
 
